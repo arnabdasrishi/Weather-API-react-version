b6a5244e0ed71414078e950f7eb4f93177e454d4
arnabdasrishi <arnabdasrishi@gmail.com>
2023-03-23 11:21:02 +0530

    data fetched

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Home from "./components/Home";
+import "./index.css";
 
 function App() {
+  const api_key = "02d239601251d949579a12127209ff8e";
+  const [data, setData] = useState({});
+  const [search, setSearch] = useState("");
+
+  const fetchData = async () => {
+    try {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=Mumbai&APPID=${api_key}&units=metric`
+        )
+        .then((response) => {
+          setData(response.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar__main">
+        <p>Home</p>
+        <p>Cities</p>
+        <p>News</p>
+      </div>
+
+      {/* Main Content area begins here  */}
+      {data.weather && (
+        <Home weather={data.weather[0].main} humidity={data.main.humidity} wind={data.wind.speed} temp={data.main.temp} city={data.name}/>
+      )}
     </div>
   );
 }
---
+++ weather-app-react/src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../index.css";
+
+const Home = ({ weather, humidity, wind, city, temp, time, img }) => {
+    let date = new Date();
+    date = date.toString().slice(0,25);
+  return (
+    <div className="home__main__div">
+      <div className="home__city__temp__details__left">
+        <h3>Weather Detials</h3>
+        <div className="temp__details">
+          <p>{weather}</p>
+          {/* Could not find the  weather condition % */}
+          <p>{humidity - 14}%</p>
+        </div>
+        <div className="temp__details">
+          <p>Humidity</p>
+          <p>{humidity}%</p>
+        </div>
+        <div className="temp__details">
+          <p>Wind</p>
+          <p>{Math.floor(wind)} km/h</p>
+        </div>
+      </div>
+
+      <div className="home__weather__details__right">
+        <h1>{Math.round(temp)}°</h1>
+        <div>
+          <h3>{city}</h3>
+          <p>{date}</p>
+        </div>
+        <p>☀️</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
